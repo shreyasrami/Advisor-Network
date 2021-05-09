35e62b40714d92734e120a8e9f28a5380eb3df5c
shreyasrami <shreyasrami24.sr@gmail.com>
2021-05-09 20:48:32 +0530

    ch(deploy): Setup deploy to heroku

--- advisor/settings.py
+++ advisor/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,5 +159,4 @@ REST_FRAMEWORK = {
 }
 
 
-
-
+django_heroku.settings(locals())
