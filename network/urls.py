a35a2b1fd13b89cc8e8fbedbb3142c1fd2573ba3
shreyasrami <shreyasrami24.sr@gmail.com>
2021-05-10 16:06:31 +0530

    ch(deploy): Remove refresh token url

--- network/urls.py
+++ network/urls.py
@@ -3,10 +3,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 from .views import AdvisorView, RegisterUserView, LoginUserView, AllBookingsView, MakeBookingView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -26,7 +23,6 @@ urlpatterns = [
     path('',schema_view.with_ui("swagger", cache_timeout=0),name="schema-swagger-ui",),
     path('user/register/', RegisterUserView.as_view(),name='register_user'),
     path('user/login/', LoginUserView.as_view(), name='token_obtain_pair'),
-    path('user/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/advisor/', AdvisorView.as_view(),name='add_advisor'),
     path('user/<user_id>/advisor/', AdvisorView.as_view(),name='view_advisors'),
     path('user/<user_id>/advisor/booking/', AllBookingsView.as_view(),name='view_bookings'),
